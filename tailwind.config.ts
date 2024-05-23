4cb7c1fdf66984e1fbf3239487e52f5826bd2afc
Paul <paul.pousset@student.junia.com>
2024-05-23 11:01:16 +0200

    a

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,7 +20,7 @@ module.exports = withMaterialColors(
     // Your base colors as HEX values. 'primary' is required.
     primary: "#F5B61F",
     secondary: "#5482BB"
-  },  
+  },
   {
     /* one of 'content', 'expressive', 'fidelity', 'monochrome', 'neutral', 'tonalSpot' or 'vibrant' */
     //scheme: "content",
