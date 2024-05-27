5bf0f8ef0b010e1ed8e0755dd60e9f2086a6c34e
Paul <paul.pousset@student.junia.com>
2024-05-27 17:38:39 +0200

    front-test to back

--- src/app/app/g/[groupId]/(main)/_components/event-card.tsx
+++ src/app/app/g/[groupId]/(main)/_components/event-card.tsx
@@ -10,7 +10,15 @@ export interface Event {
 export function EventCard({ event }: { event: Event }) {
   return (
     <>
-      <Link href={`e/${event.id}`}>{event.title}</Link>
+     
+      <div className="sortie flex justify-between items-center w-full">
+            <Link href={`e/${event.id}`} className="bg-surface-variant max-w-60 my-4 flex-grow flex w-full cursor-pointer items-center justify-start space-x-2 rounded-r-xl p-2 transition-transform hover:scale-105" style={{ minHeight: '60px' }} >
+              {event.title}
+            </Link>
+            <div className="bg-surface-variant my-4 flex-initial max-w-xs w-1/6 cursor-pointer items-center justify-center space-x-2 rounded-l-xl p-2 transition-transform hover:scale-105" style={{ minHeight: '60px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+              Date
+            </div>
+          </div>
     </>
   );
 }
