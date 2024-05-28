53f3347b3a00a448ca835ddf3558ff01d467a473
Bafbi <bafbi3@gmail.com>
2024-05-28 15:35:56 +0200

    add pusher

--- src/app/app/g/[groupId]/(main)/page.tsx
+++ src/app/app/g/[groupId]/(main)/page.tsx
@@ -1,4 +1,18 @@
-export default async function GroupMain() {
+"use client";
+
+import Pusher from "pusher-js";
+import { env } from "~/env";
+
+export default function GroupMain() {
+  const pusher = new Pusher(env.NEXT_PUBLIC_PUSHER_APP_KEY, {
+    cluster: env.NEXT_PUBLIC_PUSHER_APP_CLUSTER,
+  });
+
+  const channel = pusher.subscribe("my-channel");
+  channel.bind("my-event", function (data: any) {
+    alert(JSON.stringify(data));
+  });
+
   return (
     <>
       <main>
