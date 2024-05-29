036ba30706dc9e7f8eee768e38952fe6e775f8b3
Bafbi <bafbi3@gmail.com>
2024-05-29 10:53:54 +0200

    back for groups page

--- src/server/api/routers/group.ts
+++ src/server/api/routers/group.ts
@@ -13,32 +13,54 @@ export const groupRouter = createTRPCRouter({
   createGroup: protectedProcedure
     .input(z.object({ name: z.string(), description: z.string().optional() }))
     .mutation(async ({ ctx, input }) => {
-       const id = await ctx.db.insert(groups).values({
-        userAdmin: ctx.session.user.id,
-        createdBy: ctx.session.user.id,
-        
-        description: input.description,
-        name: input.name,
-      }).returning({groupId: groups.id })
+      const id = await ctx.db
+        .insert(groups)
+        .values({
+          userAdmin: ctx.session.user.id,
+          createdBy: ctx.session.user.id,
+          description: input.description,
+          name: input.name,
+        })
+        .returning({ groupId: groups.id });
       if (id[0] == null) return;
-       await ctx.db.insert(groupsMembers).values({
+      await ctx.db.insert(groupsMembers).values({
         userId: ctx.session.user.id,
-
         groupId: id[0].groupId,
-      })
+      });
     }),
 
-  getGroups: protectedProcedure.query(({ ctx }) => {
-    return ctx.db.query.users.findFirst({
+  getGroups: protectedProcedure.query(async ({ ctx }) => {
+    const groupsQuery = await ctx.db.query.users.findFirst({
       columns: {},
       where: (users, { eq }) => eq(users.id, ctx.session.user.id),
       with: {
         groups: {
+          columns: {},
           with: {
-            group: true,
-          }
+            group: {
+              with: {
+                createdBy: {
+                  columns: {
+                    name: true,
+                  },
+                },
+                members: {
+                  columns: {},
+                  with: {
+                    user: {
+                      columns: {
+                        name: true,
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
         },
       },
     });
+    if (groupsQuery == undefined) return null;
+    return groupsQuery.groups.map((group) => group.group);
   }),
 });
