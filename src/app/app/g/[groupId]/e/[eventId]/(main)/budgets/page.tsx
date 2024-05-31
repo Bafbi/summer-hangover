2c97af269cbe2cd4e5aa2cde4d21f67b4c868369
juenzo <julien59280@gmail.com>
2024-05-31 17:58:56 +0200

    Front Expenses Page

--- src/app/app/g/[groupId]/e/[eventId]/(main)/budgets/page.tsx
+++ src/app/app/g/[groupId]/e/[eventId]/(main)/budgets/page.tsx
@@ -13,30 +13,30 @@ export default async function BudgetPage({
     <>
       <main className="bg-surface grid grid-cols-1 gap">
       <div className="sortie flex justify-between items-center w-full">
-      <Link href="/other-content-2" className="bg-primary-container flex-col max-w-40 my-4 flex-grow flex w-2/6 cursor-pointer items-center justify-center rounded-r-xl p-2 transition-transform hover:scale-105" style={{ minHeight: '60px' }} passHref>
+      <Link href="/other-content-2" className="bg-primary-container flex-col max-w-40 my-4 flex-grow flex w-2/6 cursor-pointer items-center justify-center rounded-r-xl p-2" style={{ minHeight: '60px' }} passHref>
       <span className="material-icons">account_balance_wallet</span>
       <div>
-          Dépenses
+          Expenses
       </div>
       </Link>
-      <Link href="/other-content-2" className="bg-primary-container flex-col max-w-40 my-4 flex-grow flex w-2/6 cursor-pointer items-center justify-center rounded-l-xl p-2 transition-transform hover:scale-105" style={{ minHeight: '60px' }} passHref>
+      <Link href="/other-content-2" className="bg-primary-container flex-col max-w-40 my-4 flex-grow flex w-2/6 cursor-pointer items-center justify-center rounded-l-xl p-2 " style={{ minHeight: '60px' }} passHref>
         <span className="material-icons">price_change</span>
           <div>
-            Equilibre
+            Balance
           </div>
       </Link>
       </div>
         {expenses.map((expense) => (
           <div className="sortie flex justify-between items-center w-full" key={expense.id}>
-            <Link href="/other-content-1" className="bg-surface-variant max-w-60 my-4 flex-col flex w-full cursor-pointer justify-start rounded-r-xl p-2 transition-transform hover:scale-105" passHref>
+            <Link href="/other-content-1" className="bg-surface-variant max-w-60 my-4 flex-col flex w-full cursor-pointer justify-start rounded-r-xl p-2 " passHref>
               <div className="font-semibold">
                 {expense.label}
               </div>
               <div>
-                Par : {expense.user.name} {/* Remplacer par le nom de l'utilisateur si disponible */}
+                By : {expense.user.name} {/* Remplacer par le nom de l'utilisateur si disponible */}
               </div>
             </Link>
-            <div className="bg-surface-variant my-4 flex-initial max-w-xs w-1/6 cursor-pointer items-center justify-center space-x-2 rounded-l-xl p-2 transition-transform hover:scale-105" style={{ minHeight: '60px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <div className="bg-surface-variant my-4 flex-initial max-w-xs w-1/6 cursor-pointer items-center justify-center space-x-2 rounded-l-xl p-2" style={{ minHeight: '60px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
               {expense.amount}<span className="material-icons mx-1">euro_symbol</span>
             </div>
           </div>
@@ -44,7 +44,7 @@ export default async function BudgetPage({
         <div className="sortie flex w-full items-center justify-between">
           <Link
             href="/other-content-2"
-            className="bg-primary-container my-4 w-1/6 max-w-xs flex-initial cursor-pointer items-center justify-center space-x-2 rounded-r-xl p-2 transition-transform hover:scale-105"
+            className="bg-primary-container my-4 w-1/6 max-w-xs flex-initial cursor-pointer items-center justify-center space-x-2 rounded-r-xl p-2"
             style={{
               minHeight: "60px",
               display: "flex",
@@ -56,11 +56,11 @@ export default async function BudgetPage({
           </Link>
           <Link
             href="./budgets/create"
-            className="bg-primary-container my-4 flex w-full max-w-60 flex-grow cursor-pointer items-center justify-center space-x-2 space-x-reverse rounded-l-xl p-2 transition-transform hover:scale-105"
+            className="bg-primary-container my-4 flex w-full max-w-60 flex-grow cursor-pointer items-center justify-center space-x-2 space-x-reverse rounded-l-xl p-2"
             style={{ minHeight: "60px" }}
             passHref
           >
-            <div>Ajouter une dépense</div>
+            <div>Add expense</div>
           </Link>
         </div>
       </main>
