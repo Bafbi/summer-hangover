dd205724c0ed0e794960673ba74bbbb24537522e
Cyprien Kelma <cyprien.kelma@gmail.com>
2024-06-06 17:58:14 +0200

    + Finish the notification page + Work on custom notif type Need to work on toast notification system for all pages.

--- src/app/app/(main)/notification/page.tsx
+++ src/app/app/(main)/notification/page.tsx
@@ -1,200 +1,290 @@
-  "use client";
-  import React, { useEffect, useState } from "react";
-  import MainMenuFooter from "~/app/_components/mainMenuFooter";
-  import Head from "next/head";
-  import MainMenuHeader from "~/app/_components/mainMenuHeader";
-  import Link from "next/link";
-  import { useSession } from 'next-auth/react';
-  import { api } from "~/trpc/react";
-  import Pusher from "pusher-js";
-  import { env } from "~/env";
-  import { format, isToday, isYesterday } from 'date-fns';
+"use client";
+import { isToday, isYesterday } from "date-fns";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import Pusher from "pusher-js";
+import { useEffect, useState } from "react";
+import MainMenuHeader from "~/app/_components/mainMenuHeader";
+import { env } from "~/env";
+import { api, type RouterOutputs } from "~/trpc/react";
+import { notificationType } from "~/server/db/schema";
 
-  interface Notification {
-    id: number;
-    userId: string;
-    message: string;
-    createdAt: string; 
-  }
+function formatNotifications(
+  notifications: RouterOutputs["notification"]["getNotifications"],
+) {
+  // On trie les notifications par date
+  const todaysNotifications = notifications.filter(
+    (notif) => notif.createdAt && isToday(new Date(notif.createdAt)),
+  );
+  const yesterdaysNotifications = notifications.filter(
+    (notif) => notif.createdAt && isYesterday(new Date(notif.createdAt)),
+  );
+  const olderNotifications = notifications.filter(
+    (notif) =>
+      notif.createdAt &&
+      !isToday(new Date(notif.createdAt)) &&
+      !isYesterday(new Date(notif.createdAt)),
+  );
 
-// Fonction qui permet de formater les notifications
-  const formatNotifications = (notifications: any[]) => {
-    // On formate les notifications pour les regrouper par date
-    const formattedNotifs = notifications.reduce((accumulator, notification) => {
+  return {
+    todaysNotifications,
+    yesterdaysNotifications,
+    olderNotifications,
+  };
+}
 
-      // "date" désigne la date de la notification
-      const date = new Date(notification.createdAt);
+const pusher = new Pusher(env.NEXT_PUBLIC_PUSHER_APP_KEY, {
+  cluster: env.NEXT_PUBLIC_PUSHER_APP_CLUSTER,
+});
 
+export default function Notifications() {
+  const { data: session } = useSession();
+  const [notifications, setNotifications] = useState<
+    RouterOutputs["notification"]["getNotifications"]
+  >([]);
+  const [notificationId, setNotificationId] = useState<number>(0);
 
-      // Test si la date est invalide
-      if (isNaN(date.getTime())) {
-        console.error('Invalid date:', notification.createdAt);
-        return accumulator;
-      }
+  const { data: notificationsData } =
+    api.notification.getNotifications.useQuery();
+  const { data: notification } = api.notification.getNotification.useQuery({
+    notifId: notificationId,
+  });
+  const sendNotification = api.notification.sendNotification.useMutation();
 
-      // On formate la date pour l'afficher en tant que chaine de caractères
-      const formattedDate = isToday(date) ? "Aujourd'hui" : 
-                            (isYesterday(date) ? 'Hier' : format(date, 'eeee dd/MM/yyyy'));
-  
-      // Si la date n'existe pas dans l'accumulateur, on la créer
-      if (!accumulator[formattedDate]) {
-        accumulator[formattedDate] = [];
-      }
+  useEffect(() => {
+    if (notificationsData) {
+      setNotifications(notificationsData);
+    }
+  }, [notificationsData]);
 
-      // On ajoute la notification à la date correspondante
-      accumulator[formattedDate].push(notification);
-      return accumulator;
-    }, {});
-  
-    console.log('SIMPLE_formattedNotifications:', formattedNotifs);
-    console.log('ENTRIES_Object.entries(formattedNotifications):', Object.entries(formattedNotifs));
-    // On tri les dates par ordre décroissant pour les afficher en commencant par la plus récente
-    return Object.entries(formattedNotifs)
-      .sort((a, b) => new Date(b[0]).getTime() - new Date(a[0]).getTime());
+  useEffect(() => {
+    if (notification) {
+      setNotifications((prev) => [...prev, notification]);
+    }
+  }, [notification]);
+
+  useEffect(() => {
+    if (!session) return;
+    const userId = session.user.id;
+
+    const channel = pusher.subscribe(`notifications-${userId}`);
+
+    channel.bind("new-notification", (data: { notifId: number }) => {
+      setNotificationId(data.notifId);
+    });
+
+    return () => {
+      pusher.unsubscribe(`notifications-${userId}`);
+    };
+  }, [session]);
+
+  const handleSendNotification = () => {
+    if (!session) return;
+    const message = "Test notification INVITED";
+    sendNotification.mutate({ message, type: "INVITED_TO_GROUP" });
   };
 
+  // have the formatNotifications always up to date
+  const formattedNotifications = formatNotifications(notifications);
 
-  export default function Notifications() {
-    
-    const { data: session } = useSession();
-    const [notifications, setNotifications] = useState<any[]>([]);
-    const getNotifications = api.notification.getNotifications.useQuery();
-    const sendNotification = api.notification.sendNotification.useMutation();
+  return (
+    <div className="bg-surface flex h-screen flex-col">
+      {/* Header de la page notif */}
+      <div className="bg-surface-variant fixed left-0 right-0 top-0 z-10 flex h-16 items-center justify-between px-4 text-inverse-primary">
+        <div className="flex-1 justify-between text-inverse-surface">
+          <p className="text-2xl font-bold text-inverse-surface">
+            Notifications
+          </p>
+        </div>
+        <div className="flex justify-around space-x-7 pr-1 text-on-surface-variant">
+          <Link
+            href="notification"
+            passHref
+            className="relative flex items-center justify-center "
+          >
+            <span
+              style={{ fontSize: 40 }}
+              className="material-icons animate-shake relative animate-ping text-error"
+            >
+              notifications
+            </span>
+          </Link>
+          <Link
+            href="profile"
+            passHref
+            className="relative flex items-center justify-center "
+          >
+            <span style={{ fontSize: 40 }} className="material-icons">
+              account_circle
+            </span>
+          </Link>
+        </div>
+      </div>
 
-    useEffect(() => {
-      if (getNotifications.data) {
-        setNotifications(getNotifications.data);
-      }
-    }, [getNotifications.data]);
-
-    useEffect(() => {
-      if (!session) return;
-
-      const userId = session.user.id;
-
-      // Initialize Pusher
-      const pusher = new Pusher(env.NEXT_PUBLIC_PUSHER_APP_KEY, {
-        cluster: env.NEXT_PUBLIC_PUSHER_APP_CLUSTER || '',
-        authEndpoint: '/api/pusher/auth',
-      });
-
-      // Canal privé de l'utilisateur
-      const channel = pusher.subscribe(`private-user-${userId}`);
-
-      // Associe une fonction de rappel à l'event 'new-notification' sur le canal
-      channel.bind('new-notification', (data: { message: any; }) => {
-        setNotifications((prevNotifications) => [
-          ...prevNotifications,
-          data.message,
-        ]);
-      });
-
-      // Nettoyage et fin d'écoute du canal
-      return () => {
-        pusher.unsubscribe(`private-user-${userId}`);
-      };
-    }, [session]);
-
-    const handleSendNotification = async () => {
-      if (!session) return;
-      const message = "Ceci est un test de notification";
-      await sendNotification.mutateAsync({ message });
-    };
-
-    const formattedNotifications = formatNotifications(notifications);
-    
-    return (
-      <div className="bg-surface flex h-screen flex-col">
-        <MainMenuHeader />
-
-        {/* Contenu de la page notif */}
-        <main className="bg-surface mt-12 flex-grow overflow-y-auto overflow-x-hidden px-2 py-4">
-
-          {/* Titre de la section notif + bouton retour et marqué comme lu */}
-          <div className="bg-surface-variant mb-4 mt-6 flex h-14 flex-col items-center rounded-md">
-            <h1 className="pt-2 text-4xl font-semibold">Notifications</h1>
-          </div>
-          <button onClick={handleSendNotification}>Envoyer une notification</button>
-          <p>session.user.id = {session?.user.id}</p>
-          <div className="mb-8 ml-52 mt-3 flex items-center justify-between">
-            <div className="flex items-center space-x-1">
-              <button className="mr-3 rounded-md bg-inverse-primary px-5 py-1 text-sm font-semibold text-inverse-surface">
-                <span>
-                  Marquer
-                  <br />
-                  comme lu
-                </span>
-              </button>
-              <Link
-                href="/app"
-                className="rounded-full bg-inverse-primary p-2 font-bold"
-              >
-                <span style={{ fontSize: 40 }} className="material-icons">
-                  close
-                </span>
-              </Link>
-            </div>
-          </div>
-
+      {/* Contenu de la page notif */}
+      <main className="bg-surface mt-12 flex-grow overflow-y-auto overflow-x-hidden  px-2 py-4">
+        <div className="notifContent">
+          {formattedNotifications.todaysNotifications.length === 0 &&
+            formattedNotifications.yesterdaysNotifications.length === 0 &&
+            formattedNotifications.olderNotifications.length === 0 && (
+              <div className="flex flex-grow flex-col items-center justify-center text-on-surface-variant">
+                <div className="logo justify-center pt-10">
+                  <span
+                    style={{ fontSize: 105 }}
+                    className="material-icons items-center justify-between"
+                  >
+                    circle_notifications
+                  </span>
+                </div>
+                <div className="bg-surface-variant mb-4 mt-0 h-12 rounded-md px-5">
+                  <h1 className="pt-2 text-xl font-bold">
+                    Aucune Notification
+                  </h1>
+                </div>
+                <div
+                  className="mx-7 mb-24 mt-10 flex items-center justify-between gap-4
+                rounded-md bg-on-inverse-surface px-3 py-2 text-lg text-on-surface-variant"
+                >
+                  <p className="text-center text-base font-semibold">
+                    Vous n'avez pas de notification pour le moment. Lorsque vous
+                    en recevrez, elles apparaîtront ici.
+                  </p>
+                </div>
+              </div>
+            )}
           {/* Section des notifications */}
-          {/* TODO : Faire le front des notif + séparation selon leurs dates (auj, hier, antèrieur)*/}
-          {formattedNotifications.map(([date, items], index) => (
-            <div key={index}>
-              <h1>{date}</h1>
-              {items.map((notification, idx) => (
-                <div key={idx} className="notification-item">
-                  {notification.message}
+          {/* Aujourd'hui */}
+          {formattedNotifications.todaysNotifications.length > 0 && (
+            <>
+              <div className="bg-surface-variant mb-4 mt-6 flex h-10 flex-col items-center rounded-md">
+                <h1 className="pt-1 text-2xl font-semibold">{"Aujourd'hui"}</h1>
+              </div>
+              {formattedNotifications.todaysNotifications.map((notif) => (
+                <div
+                  key={notif.id}
+                  className="mx-3 my-3 flex items-center justify-between gap-4 rounded-md bg-on-inverse-surface px-3 py-2 text-lg"
+                >
+                  <span
+                    style={{ fontSize: 36 }}
+                    className="material-icons pl-1 text-error"
+                  >
+                    report
+                  </span>
+                  <div className="flex items-center space-x-2">
+                    {notif.message.charAt(0).toUpperCase() +
+                      notif.message.slice(1)}
+                  </div>
                 </div>
               ))}
-            </div>
-          ))}
+            </>
+          )}
 
-          /* TODO : le contenue de chaque notification de formattedNotifications de la manière suivante:
-           Aujourd'hui :
-           - NOM1 a ajouté un nouvel évènement au groupe (exemple)
-           Hier :
-           - NOM2 vous a invité à rejoindre son groupe (exemple)
-           - NOM3 a ajouté une nouvelle activité à l'évènement (exemple)
-           Mardi 07/06/2024 :
-           - NOM4 a ajouté un nouvel évènement au groupe (exemple)*/
-          
-          
-        </main>
-      </div>
-    );
-  };
+          {/* Hier */}
+          {formattedNotifications.yesterdaysNotifications.length > 0 && (
+            <>
+              <div className="mb-4 mt-8 flex h-10 flex-col items-center rounded-md bg-surface-variant text-on-surface-variant">
+                <h1 className="pt-1 text-2xl font-semibold">Hier</h1>
+              </div>
+              <div>
+                {formattedNotifications.yesterdaysNotifications.map((notif) => (
+                  <div
+                    key={notif.id}
+                    className="mx-3 my-3 flex items-center justify-between gap-4 rounded-md bg-on-inverse-surface px-3 py-2 text-lg text-inverse-surface"
+                  >
+                    <span
+                      style={{ fontSize: 36 }}
+                      className="material-icons pl-1 text-error"
+                    >
+                      report
+                    </span>
+                    <div
+                      key={notif.id}
+                      className="flex items-center justify-between"
+                    >
+                      <div className="flex items-center space-x-2 rounded-md px-3 py-2 text-lg">
+                        {notif.message.charAt(0).toUpperCase() +
+                          notif.message.slice(1)}
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </>
+          )}
 
+          {/* Anterieur */}
+          {formattedNotifications.olderNotifications.length > 0 && (
+            <>
+              <div className="bg-surface-variant mb-4 mt-6 flex h-10 flex-col items-center rounded-md">
+                <h2 className="pt-1 text-2xl font-semibold">Antérieur</h2>
+              </div>
+              {formattedNotifications.olderNotifications
+                .filter((notif) => notif.createdAt !== null) // Null check
+                .sort((a, b) => (a.createdAt! < b.createdAt! ? 1 : -1)) // non-null assertion operator
+                .map((notif, index) => {
+                  if (index < 5) {
+                    return (
+                      <div
+                        key={notif.id}
+                        className="mx-3 my-3 flex items-center justify-between gap-4 rounded-md bg-on-inverse-surface px-3 py-2 text-lg text-inverse-surface"
+                      >
+                        <span
+                          style={{ fontSize: 36 }}
+                          className="material-icons pl-1 text-on-secondary-container"
+                        >
+                          report
+                        </span>
+                        <div>
+                          {notif.message}
+                          <span className="inline text-sm italic text-on-surface-variant">
+                            {" " +
+                              notif.createdAt
+                                ?.toLocaleDateString("fr-FR", {
+                                  weekday: "long",
+                                  year: "2-digit",
+                                  month: "2-digit",
+                                  day: "2-digit",
+                                })
+                                .replace(/^\w/, (c) => c.toUpperCase())}
+                          </span>
+                        </div>
+                      </div>
+                    );
+                  } else {
+                    return null;
+                  }
+                })}
+            </>
+          )}
+        </div>
 
+        {/* Fin de la section des notifications */}
 
+        {/* Pour harmoniser le bouton retour */}
+        {formattedNotifications.todaysNotifications.length != 0 &&
+          formattedNotifications.todaysNotifications.length <= 5 &&
+          formattedNotifications.yesterdaysNotifications.length === 0 &&
+          formattedNotifications.olderNotifications.length === 0 && (
+            <div className="h-28"></div>
+          )}
 
-  /* 
-    useEffect(() => {
-      const userId = session?.user?.id;
-
-      // Permet de se connecter au service Pusher, avec nos informations d'identification
-      const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY ?? "", {
-        cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER ?? "",
-        authEndpoint: "/api/pusher/auth", // Pour l'authentification privée
-      });
-
-      // On s'abonne au canal privé de l'utilisateur :
-      const channel = pusher.subscribe(`private-user-${userId}`); // Remplacez par l'ID utilisateur dynamique
-
-      // Permet de lier une fonction de rappel à un événement spécifique sur le canal.
-      // On écoute l'événement 'new-notification' sur le canal. lorsque l'événement est
-      // reçu, on applique setNotification qui ajoute la notification à la liste des notifications
-      channel.bind("new-notification", 
-        (data: { message: any }) => {
-          setNotifications((prevNotifications) => [
-            // Produit un nouveau tableau en concaténant les notifications 
-            // précédentes avec la nouvelle notification
-            ...prevNotifications,
-            data.message,
-          ]);
-      });
-
-      return () => {
-        pusher.unsubscribe(`private-user-${userId}`);
-      };
-    }, [session]);
-  */
+        <div className="mt-6 justify-self-end">
+          <div className="flex items-center justify-center">
+            <Link
+              href="/app"
+              className="my-4 items-center rounded-3xl bg-inverse-primary px-10 py-3 text-xl font-semibold text-inverse-surface"
+            >
+              <span>Retour</span>
+            </Link>
+          </div>
+          <div>
+            <button className="mt-40" onClick={handleSendNotification}>
+              Envoyer une notification
+            </button>
+            <p>session.user.id = {session?.user.id}</p>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
--- src/server/api/routers/notifications.ts
+++ src/server/api/routers/notifications.ts
@@ -1,54 +1,55 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
-import Pusher from "pusher";
-import { eq } from "drizzle-orm";
-import { notifications, users } from "~/server/db/schema";
-import { db } from "~/server/db/index";
+import { eq, not } from "drizzle-orm";
+import { notifications, notificationType } from "~/server/db/schema";
+import { pusher } from "~/server/pusher";
+import Notifications from "~/app/app/(main)/notification/page";
 
-// Informations d'identification de Pusher
-const pusher = new Pusher({
-    appId: "1809412", // process.env.PUSHER_APP_ID
-    key: "1732a4efe6a8b6f7c753", // process.env.PUSHER_KEY
-    secret: "dba36f16e80011ac0136", //process.env.PUSHER_SECRET
-    cluster: "eu", // process.env.PUSHER_CLUSTER
-    useTLS: true, // process.env.PUSHER_TLS
-});
-
-// Routeur TRPC pour les notifications
-// Les notifications sont envoyées à un utilisateur spécifique
-// et stockées dans la base de données
 export const notificationRouter = createTRPCRouter({
-
-    sendNotification: protectedProcedure
-      .input(z.object({
+  sendNotification: protectedProcedure
+    .input(
+      z.object({
         message: z.string(),
-      }))
-      .mutation(async ({ ctx, input }) => {
-        console.log("Session:", ctx.session);
-        const user = await db.select().from(users).where(eq(users.id, ctx.session.user.id));
-  
-        if (!user) {
-          throw new Error('Utilisateur non trouvé');
-        }
-  
-        const userId = ctx.session.user.id;
-  
-        // Enregistre la notif dans la base de données :
-        await db.insert(notifications).values({
-            userId: userId,
-            message: input.message,
-            createdAt: new Date(),
-        });
-  
-        // Déclenche l'événement 'new-notification' sur le canal 'private-user-{userId}'
-        await pusher.trigger(`private-user-${userId}`, 'new-notification', {
-          message: input.message,
-        });
-  
-        return { message: 'Notification envoyée avec succès' };
+        type: z.enum(notificationType),
       }),
-  
-    getNotifications: protectedProcedure.query(async ({ ctx }) => {
-      return db.select().from(notifications).where(eq(notifications.userId, ctx.session.user.id));
+    )
+    .mutation(async ({ ctx, input }) => {
+      const id = await ctx.db
+        .insert(notifications)
+        .values({
+          userId: ctx.session.user.id,
+          message: input.message,
+          createdAt: new Date(),
+          isRead: false,
+          notifType: input.type,
+        })
+        .returning({ id: notifications.id });
+
+      if (id[0] == null) return;
+      const notifId = id[0].id;
+
+      // Déclenche l'événement 'new-notification' sur le canal 'private-user-{userId}'
+      await pusher.trigger(
+        `notifications-${ctx.session.user.id}`,
+        "new-notification",
+        {
+          notifId,
+        },
+      );
     }),
-  });
+
+  getNotification: protectedProcedure
+    .input(z.object({ notifId: z.number() }))
+    .query(async ({ ctx, input }) => {
+      return ctx.db.query.notifications.findFirst({
+        where: (notifications, { eq }) => eq(notifications.id, input.notifId),
+      });
+    }),
+
+  getNotifications: protectedProcedure.query(async ({ ctx }) => {
+    return ctx.db
+      .select()
+      .from(notifications)
+      .where(eq(notifications.userId, ctx.session.user.id));
+  }),
+});
