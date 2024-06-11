24c51533ec40db31729da792d9394c16696c8c1f
Bafbi <bafbi3@gmail.com>
2024-06-11 12:29:51 +0200

    example aggregate

--- src/app/app/g/[groupId]/(main)/hallOfFame/page.tsx
+++ src/app/app/g/[groupId]/(main)/hallOfFame/page.tsx
@@ -12,61 +12,77 @@ interface Card {
   isFlipped: boolean;
 }
 
-export default function HofPage({params}: {params: {groupId: string}}) {
-
+export default function HofPage({ params }: { params: { groupId: string } }) {
   // Récupérer les données de l'API
-  const topSpends = api.allOfFame.topSpend.useQuery({groupId: +params.groupId});
+  const topSpends = api.allOfFame.topSpend.useQuery({
+    groupId: +params.groupId,
+  });
+  const mostParticipant = api.allOfFame.mostParticipant.useQuery({
+    groupId: +params.groupId,
+  });
 
   const [cards, setCards] = useState<Card[]>([]);
 
   useEffect(() => {
     if (topSpends.data) {
-      const initialCards: Omit<Card, 'isFlipped'>[] = [
-        { id: 1, question: 'Qui a fais la plus grosse dépense ?', answer: `${topSpends.data.user.name} with ${topSpends.data.amount}€` },
-        { id: 2, question: 'Question 2 ?', answer: '2' },
-        { id: 3, question: 'Question 3 ?', answer: '3' },
-        { id: 4, question: 'Question 4 ?', answer: '4' },
-        { id: 5, question: 'Question 5 ?', answer: '5' }
+      const initialCards: Omit<Card, "isFlipped">[] = [
+        {
+          id: 1,
+          question: "Qui a fais la plus grosse dépense ?",
+          answer: `${topSpends.data.user.name} with ${topSpends.data.amount}€`,
+        },
+        { id: 2, question: "Question 2 ?", answer: "2" },
+        { id: 3, question: "Question 3 ?", answer: "3" },
+        { id: 4, question: "Question 4 ?", answer: "4" },
+        { id: 5, question: "Question 5 ?", answer: "5" },
       ];
-      setCards(initialCards.map(card => ({ ...card, isFlipped: false })));
+      setCards(initialCards.map((card) => ({ ...card, isFlipped: false })));
     }
   }, [topSpends.data]);
 
   const handleFlip = (id: number) => {
-    setCards(cards.map(card => 
-      card.id === id ? { ...card, isFlipped: !card.isFlipped } : card
-    ));
+    setCards(
+      cards.map((card) =>
+        card.id === id ? { ...card, isFlipped: !card.isFlipped } : card,
+      ),
+    );
   };
 
   if (!topSpends.data) {
     return null; // or any other appropriate action
   }
-  
+
   return (
     <>
-    <div className="max-w-80 mx-auto p-2  bg-surface-dim text-center cursor-pointer shadow-md rounded-b-lg transition flex justify-center items-center">
-     <span className="material-icons text-primary">emoji_events</span><div className="text-lg font-bold mx-4">Hall Of Fame</div><span className="material-icons text-primary">emoji_events</span>
-    </div>
-    <main className="grid grid-cols-2 gap-4 py-4 px-8 bg-surface">
-      {cards.map(card => (
-        <div key={card.id} className="card-container transition flex justify-center items-center" onClick={() => handleFlip(card.id)}>
-          <div className={`card ${card.isFlipped ? 'flipped' : ''}`}>
-            <div className="front bg-surface-variant rounded-lg max-w-lg w-full" >
-            <div className="award-section bg-secondary-container font-semibold">
-            {/* <span className="material-icons text-primary">military_tech</span> */}
-            </div>
-              <div className="text-lg font-bold m-auto">{card.question}</div>
-              <div className="answer-section bg-secondary-container">
-                <span className="material-icons">touch_app</span>
+      <div className="bg-surface-dim mx-auto flex  max-w-80 cursor-pointer items-center justify-center rounded-b-lg p-2 text-center shadow-md transition">
+        <span className="material-icons text-primary">emoji_events</span>
+        <div className="mx-4 text-lg font-bold">Hall Of Fame</div>
+        <span className="material-icons text-primary">emoji_events</span>
+      </div>
+      <main className="bg-surface grid grid-cols-2 gap-4 px-8 py-4">
+        {cards.map((card) => (
+          <div
+            key={card.id}
+            className="card-container flex items-center justify-center transition"
+            onClick={() => handleFlip(card.id)}
+          >
+            <div className={`card ${card.isFlipped ? "flipped" : ""}`}>
+              <div className="front bg-surface-variant w-full max-w-lg rounded-lg">
+                <div className="award-section bg-secondary-container font-semibold">
+                  {/* <span className="material-icons text-primary">military_tech</span> */}
+                </div>
+                <div className="m-auto text-lg font-bold">{card.question}</div>
+                <div className="answer-section bg-secondary-container">
+                  <span className="material-icons">touch_app</span>
+                </div>
+              </div>
+              <div className="back bg-primary-container text-primary">
+                <div className="text-lg font-bold">{card.answer}</div>
               </div>
             </div>
-            <div className="back bg-primary-container text-primary">
-              <div className="text-lg font-bold">{card.answer}</div>
-            </div>
           </div>
-        </div>
-      ))}
-    </main>
+        ))}
+      </main>
     </>
   );
 }
--- src/server/api/routers/all-of-fame.ts
+++ src/server/api/routers/all-of-fame.ts
@@ -1,22 +1,36 @@
-import { and, eq, sql } from "drizzle-orm";
+import { and, count, eq, sql } from "drizzle-orm";
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
-import { expenses, users } from "~/server/db/schema";
+import {
+  events,
+  eventsParticipants,
+  expenses,
+  users,
+} from "~/server/db/schema";
 
 export const allOfFameRouter = createTRPCRouter({
-  
-    // top spend of a group
+  // top spend of a group
+  topSpend: protectedProcedure
+    .input(z.object({ groupId: z.number() }))
+    .query(async ({ ctx, input }) => {
+      const topSpend = await ctx.db.query.expenses.findFirst({
+        where: (expenses, { eq, and }) => eq(expenses.groupId, input.groupId),
+        with: {
+          user: true,
+        },
+        orderBy: (expenses, { desc }) => [desc(expenses.amount)],
+      });
 
-    topSpend: protectedProcedure.input(z.object({ groupId: z.number()})).query(async ({ ctx, input }) => {
-        const topSpend = await ctx.db.query.expenses.findFirst({
-            where: (expenses, { eq, and }) => eq(expenses.groupId, input.groupId),
-            with: {
-                user: true,
-            },
-            orderBy: (expenses, { desc }) => [desc(expenses.amount)],
-        });
-
-        return topSpend;
+      return topSpend;
     }),
 
+  mostParticipant: protectedProcedure
+    .input(z.object({ groupId: z.number() }))
+    .query(async ({ ctx, input }) => {
+      return ctx.db
+        .select({ user: users, count: count(eventsParticipants.eventId) })
+        .from(users)
+        .innerJoin(eventsParticipants, eq(users.id, eventsParticipants.userId))
+        .groupBy(users.id);
+    }),
 });
