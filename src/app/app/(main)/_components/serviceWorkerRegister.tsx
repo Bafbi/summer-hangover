9a6ced6783f08e6018b87aebd6682ddf75923a43
Bafbi <bafbi3@gmail.com>
2024-06-13 12:39:08 +0200

    ficx linting again

--- src/app/app/(main)/_components/serviceWorkerRegister.tsx
+++ src/app/app/(main)/_components/serviceWorkerRegister.tsx
@@ -1,34 +1,34 @@
 "use client";
 import { useEffect } from "react";
 
+export async function ServiceWorkerRegister() {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/service-worker.js")
+        .then(function (registration) {
+          console.log("Service Worker Registered", registration);
+        })
+        .catch(function (error) {
+          console.log("Service Worker Registration failed", error);
+        });
+    }
+  }, []);
 
-export function ServiceWorkerRegister() {
-    useEffect(() => {
-      if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/service-worker.js')
-          .then(function(registration) {
-            console.log('Service Worker Registered', registration);
-          })
-          .catch(function(error) {
-            console.log('Service Worker Registration failed', error);
-          });
+  const requestPermission = async () => {
+    try {
+      const permission = await Notification.requestPermission();
+      if (permission === "granted") {
+        console.log("Notification permission granted.");
+      } else {
+        console.log("Notification permission not granted.");
       }
-    }, []);
+    } catch (error) {
+      console.error("Error getting notification permission", error);
+    }
+  };
 
-    const requestPermission = async () => {
-      try {
-        const permission = await Notification.requestPermission();
-        if (permission === 'granted') {
-          console.log('Notification permission granted.');
-        } else {
-          console.log('Notification permission not granted.');
-        }
-      } catch (error) {
-        console.error('Error getting notification permission', error);
-      }
-    };
-  
-    requestPermission();
-  
-    return null;
-  }
+  await requestPermission();
+
+  return null;
+}
