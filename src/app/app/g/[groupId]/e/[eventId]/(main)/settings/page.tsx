1cbad7dfb32e28281f51d83767d1cdd8a0c99cb7
Paul <paul.pousset@student.junia.com>
2024-06-19 15:19:05 +0200

    page settings avec un peu de style mais pas beau

--- src/app/app/g/[groupId]/e/[eventId]/(main)/settings/page.tsx
+++ src/app/app/g/[groupId]/e/[eventId]/(main)/settings/page.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { api } from "~/trpc/react";
 import { useSession } from "next-auth/react";
+
 export default function EventSettingsPage({
   params,
 }: {
@@ -38,12 +39,14 @@ export default function EventSettingsPage({
       if (event.location) setLocation(event.location);
     }
   }, [event]);
+
   if (!session.data) return null;
+
   return (
     <>
       {event?.createdBy.id !== session.data.user.id && (
         <>
-          <h2>Information of the event</h2>
+          <h1 className="bg-surface-variant text-xl mt-2 font-bold sortie flex flex-col w-full items-center justify-between">Event infos</h1>
           <p>Description: {event?.description}</p>
           <p>Date: {event?.date?.toLocaleString()}</p>
           <p>Event Location: {event?.location}</p>
@@ -51,65 +54,84 @@ export default function EventSettingsPage({
           <p>Event Created By: {event?.createdBy.name}</p>
         </>
       )}
-      
+
       {event?.createdBy.id === session.data.user.id && (
         <>
-          <form
-            onSubmit={async (e) => {
-              e.preventDefault();
-              await updateEvent.mutate({
-                name: name,
-                description: description,
-                date: date.concat(":00Z"),
-                location: location,
-                groupId: +params.groupId,
-                eventId: +params.eventId,
-                endVoteDate: EndVoteDate.concat(":00Z"),
-              });
-            }}
-          >
-            Name:
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <br />
-            Description:
-            <input
-              type="text"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-            <br />
-            Date:
-            <input
-              type="datetime-local"
-              value={date}
-              onChange={(e) => setDate(e.target.value)}
-            />
-            <br />
-            Location:
-            <input
-              type="text"
-              value={location}
-              onChange={(e) => setLocation(e.target.value)}
-            />
-            <br />
-            End Vote Date:
-            <input
-              type="datetime-local"
-              value={EndVoteDate}
-              onChange={(e) => setEndVoteDate(e.target.value)}
-            />
-            <button type="submit">Update</button>
-            <p>Event Created By: {event?.createdBy.name}</p>
-          </form>
+          <h1 className="bg-surface-variant text-xl mt-2 font-bold sortie flex flex-col w-full items-center justify-between">Event Settings</h1>
+          <div className="flex flex-col gap-5 justify-center items-center">
+            <form
+              className="w-full max-w-lg"
+              onSubmit={async (e) => {
+                e.preventDefault();
+                await updateEvent.mutate({
+                  name: name,
+                  description: description,
+                  date: date.concat(":00Z"),
+                  location: location,
+                  groupId: +params.groupId,
+                  eventId: +params.eventId,
+                  endVoteDate: EndVoteDate.concat(":00Z"),
+                });
+              }}
+            >
+              <div className="mb-4">
+                <label className="font-semibold">Name:</label>
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="peer bg-surface-container w-full rounded-md border-outline transition focus:border-tertiary focus:ring-tertiary"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="font-semibold">Description:</label>
+                <input
+                  type="text"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                  className="peer bg-surface-container w-full rounded-md border-outline transition focus:border-tertiary focus:ring-tertiary"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="font-semibold">Date:</label>
+                <input
+                  type="datetime-local"
+                  value={date}
+                  onChange={(e) => setDate(e.target.value)}
+                  className="peer bg-surface-container w-full rounded-md border-outline transition focus:border-tertiary focus:ring-tertiary"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="font-semibold">Location:</label>
+                <input
+                  type="text"
+                  value={location}
+                  onChange={(e) => setLocation(e.target.value)}
+                  className="peer bg-surface-container w-full rounded-md border-outline transition focus:border-tertiary focus:ring-tertiary"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="font-semibold">End Vote Date:</label>
+                <input
+                  type="datetime-local"
+                  value={EndVoteDate}
+                  onChange={(e) => setEndVoteDate(e.target.value)}
+                  className="peer bg-surface-container w-full rounded-md border-outline transition focus:border-tertiary focus:ring-tertiary"
+                />
+              </div>
+              <p className="font-semibold">Event Created By: {event?.createdBy.name}</p>
+              <div className="flex justify-center mt-4">
+                <button type="submit" className="rounded-3xl bg-secondary-container px-10 py-2 text-xl font-semibold text-center">
+                  Update
+                </button>
+              </div>
+            </form>
+          </div>
         </>
       )}
       <div>
-        <h2>Participants:</h2>
-        <ul>
+        <h2 className="bg-surface-variant text-xl mt-2 font-bold sortie flex flex-col w-full items-center justify-between">Members</h2>
+        <ul className="w-11/12 flex flex-col items-center">
           {participant?.map((participants, index) => (
             <li key={index}>{participants.user.name}</li>
           ))}
