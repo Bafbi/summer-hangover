4e24038f1cd434091a0e9348ca76c5e15e03747c
Cyprien Kelma <cyprien.kelma@gmail.com>
2024-06-20 10:42:06 +0200

    minor changes on admin page

--- src/server/api/routers/admin.ts
+++ src/server/api/routers/admin.ts
@@ -1,4 +1,6 @@
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import { count, sql } from 'drizzle-orm';
+import { users } from "~/server/db/schema";
 
 export const adminRouter = createTRPCRouter({
 
@@ -14,5 +16,15 @@ export const adminRouter = createTRPCRouter({
     return user.isAdmin;
   }),
 
-
+  // Pour récupérer le nombre d'utilisateurs dans la base de données
+  getUsersCount: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.db.select({count: count()}).from(users);
+  }),
+  
+  // Pour récupérer le nombre d'utilisateurs dit "actif" dans la base de données
+  // C'est à dire ceux qui ont une session active vieille de moins de 7 jours :
+  getActiveUsersCount: protectedProcedure.query(async ({ ctx }) => {
+    // TODO : Fix that piece of shit
+    return await ctx.db.select({count: count()}).from(users).where(sql`lastSeenAt > NOW() - INTERVAL '7 days'`);
+  }),
 });
