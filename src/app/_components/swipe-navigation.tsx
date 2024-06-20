07668f7cdf1e8f213b35ebd26d0f1fc28573e885
Bafbi <bafbi3@gmail.com>
2024-06-20 11:19:04 +0200

    add swiping navigation

---
+++ src/app/_components/swipe-navigation.tsx
@@ -0,0 +1,125 @@
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+// swipe in a direction to navigate to the next page or the previous page
+export function SwipeNavigation({
+  pages,
+  pageIndex,
+}: {
+  pages: readonly string[];
+  pageIndex: number;
+}) {
+  const router = useRouter();
+
+  const [touchStart, setTouchStart] = useState(0);
+
+  function handleSwipe(direction: "left" | "right") {
+    if (direction === "left") {
+      if (pageIndex < pages.length - 1) {
+        if (!document.startViewTransition) {
+          router.push(pages[pageIndex + 1]);
+        }
+        const transition = document.startViewTransition(() =>
+          router.push(pages[pageIndex + 1]),
+        );
+        // Wait for the pseudo-elements to be created:
+        transition.ready.then(() => {
+          // Animate the root’s new view
+          document.documentElement.animate(
+            {
+              transform: ["translateX(100%)", "translateX(0)"],
+            },
+            {
+              duration: 200,
+              easing: "ease-in",
+              // Specify which pseudo-element to animate
+              pseudoElement: "::view-transition-new(root)",
+            },
+          );
+          document.documentElement.animate(
+            {
+              transform: ["translateX(0)", "translateX(-100%)"],
+            },
+            {
+              duration: 200,
+              easing: "ease-in",
+              // Specify which pseudo-element to animate
+              pseudoElement: "::view-transition-old(root)",
+            },
+          );
+        });
+      }
+    } else {
+      if (pageIndex > 0) {
+        if (!document.startViewTransition) {
+          router.push(pages[pageIndex - 1]);
+        }
+        const transition = document.startViewTransition(() =>
+          router.push(pages[pageIndex - 1]),
+        );
+        // Wait for the pseudo-elements to be created:
+        transition.ready.then(() => {
+          // Animate the root’s new view
+          document.documentElement.animate(
+            {
+              transform: ["translateX(-100%)", "translateX(0)"],
+            },
+            {
+              duration: 200,
+              easing: "ease-in",
+              // Specify which pseudo-element to animate
+              pseudoElement: "::view-transition-new(root)",
+            },
+          );
+          document.documentElement.animate(
+            {
+              transform: ["translateX(0)", "translateX(100%)"],
+            },
+            {
+              duration: 200,
+              easing: "ease-in",
+              // Specify which pseudo-element to animate
+              pseudoElement: "::view-transition-old(root)",
+            },
+          );
+        });
+      }
+    }
+  }
+
+  return (
+    <div
+      className="absolute bottom-0 left-0 right-0 top-0 flex h-full w-full items-center justify-between"
+      onTouchStart={(e) => {
+        const touch = e.touches[0];
+        if (touch === undefined) return;
+        setTouchStart(touch.clientX);
+      }}
+      onTouchEnd={(e) => {
+        const touch = e.changedTouches[0];
+        if (touch === undefined) return;
+        const diff = touch.clientX - touchStart;
+        if (diff > 100) {
+          handleSwipe("right");
+        } else if (diff < -100) {
+          handleSwipe("left");
+        }
+      }}
+    >
+      {/* <button
+        onClick={() => handleSwipe("right")}
+        disabled={page === 0}
+        className="text-blue-500"
+      >
+        Previous
+      </button>
+      <button
+        onClick={() => handleSwipe("left")}
+        disabled={page === pages.length - 1}
+        className="text-blue-500"
+      >
+        Next
+      </button> */}
+    </div>
+  );
+}
--- src/app/app/g/[groupId]/e/[eventId]/(main)/_components/footer.tsx
+++ src/app/app/g/[groupId]/e/[eventId]/(main)/_components/footer.tsx
@@ -2,43 +2,45 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { SwipeNavigation } from "~/app/_components/swipe-navigation";
 
-export enum EventSection {
-  CHAT = "chat",
-  ACTIVITIES = "activities",
-  BUDGETS = "budgets",
-}
+const eventSections = ["budgets", "chat", "activities"] as const;
 
 export function EventFooter() {
   const pathname = usePathname();
+  const page = eventSections.find((section) => pathname.includes(section));
+  const pageIndex = eventSections.indexOf(page ?? "chat");
 
   return (
-    <footer className="bg-surface sticky bottom-0 ">
-      <div className="mx-2 flex h-16 flex-row items-center justify-around border-inverse-surface">
-        <Link href={`${EventSection.BUDGETS}`} replace={true}>
-          <span
-            className={`material-icons ${pathname.includes(EventSection.BUDGETS) ? "text-primary" : ""}`}
-          >
-            account_balance_wallet
-          </span>
-        </Link>
+    <>
+      <SwipeNavigation pages={eventSections} pageIndex={pageIndex} />
+      <footer className="bg-surface sticky bottom-0 ">
+        <div className="mx-2 flex h-16 flex-row items-center justify-around border-inverse-surface">
+          <Link href={`${eventSections[0]}`} replace={true}>
+            <span
+              className={`material-icons ${pageIndex === 0 ? "text-primary" : ""}`}
+            >
+              account_balance_wallet
+            </span>
+          </Link>
 
-        <Link href={`${EventSection.CHAT}`} replace={true}>
-          <span
-            className={`material-icons ${pathname.includes(EventSection.CHAT) && "text-primary"}`}
-          >
-            chat
-          </span>
-        </Link>
+          <Link href={`${eventSections[1]}`} replace={true}>
+            <span
+              className={`material-icons ${pageIndex === 1 && "text-primary"}`}
+            >
+              chat
+            </span>
+          </Link>
 
-        <Link href={`${EventSection.ACTIVITIES}`} replace={true}>
-          <span
-            className={`material-icons ${pathname.includes(EventSection.ACTIVITIES) ? "text-primary" : ""}`}
-          >
-            celebration
-          </span>
-        </Link>
-      </div>
-    </footer>
+          <Link href={`${eventSections[2]}`} replace={true}>
+            <span
+              className={`material-icons ${pageIndex === 2 ? "text-primary" : ""}`}
+            >
+              celebration
+            </span>
+          </Link>
+        </div>
+      </footer>
+    </>
   );
 }
